6d9e901550199eb25deb0f667c16f78a677a379b
saulprl <phasecross@hotmail.com>
2024-03-08 21:28:26 -0700

    Add Web Bluetooth API usage

--- src/routes/app/index.tsx
+++ src/routes/app/index.tsx
@@ -1,14 +1,66 @@
+import { Buffer } from "buffer";
+
+import axios from "axios";
+import { useUser } from "reactfire";
+
 import { Dashboard } from "@/components/dashboard/dashboard";
 import { Header } from "@/components/header/header";
+import { Button } from "@/components/ui/button";
 import { RoomSelector } from "@/components/ui/room-selector";
+import { Bluetooth } from "lucide-react";
 
 export const AppIndex = () => {
+  const { data: userData } = useUser();
+
+  const testBluetooth = async () => {
+    try {
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [{ services: ["4655318c-0b41-4725-9c64-44f9fb6098a2"] }],
+      });
+
+      const res = await axios.get(
+        `${import.meta.env.VITE_ACCESS_API_URL}/api/users/generate-token`,
+        {
+          headers: {
+            Authorization: `Bearer ${await userData?.getIdToken()}`,
+          },
+        },
+      );
+
+      const token = res.data?.token;
+
+      const server = await device.gatt?.connect();
+      const service = await server?.getPrimaryService(
+        import.meta.env.VITE_BLE_SERVICE_UUID,
+      );
+      const characteristic = await service?.getCharacteristic(
+        import.meta.env.VITE_BLE_CHARACTERISTIC_UUID,
+      );
+
+      const encodedToken = Buffer.from(token).toString("base64");
+      await characteristic?.writeValue(new TextEncoder().encode(encodedToken));
+
+      server.disconnect();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex min-h-screen w-full flex-col items-center bg-white">
       <Header title="Home" />
-      <main className="flex h-full w-full flex-col items-center gap-2 p-2">
+      <main className="relative flex h-full w-full flex-col items-center gap-2 p-2">
         <RoomSelector />
         <Dashboard />
+        {!!navigator.bluetooth && !!userData && (
+          <Button
+            onClick={testBluetooth}
+            size="icon"
+            className="absolute bottom-4 right-4 rounded-full hover:bg-primary focus:bg-primary"
+          >
+            <Bluetooth size="24" />
+          </Button>
+        )}
       </main>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import path from "path";
 
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      manifest: {
+        name: "CSI PRO Access Web",
+        short_name: "Access Web",
+        theme_color: "#7145d6",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
