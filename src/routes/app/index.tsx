eda5e2bf82cf8fd6a18e09ca068852d23352330a
saulprl <phasecross@hotmail.com>
2024-03-08 21:49:42 -0700

    Fix floating action button

--- src/routes/app/index.tsx
+++ src/routes/app/index.tsx
@@ -13,9 +13,12 @@ export const AppIndex = () => {
   const { data: userData } = useUser();
 
   const testBluetooth = async () => {
+    const serviceUuid = import.meta.env.VITE_BLE_SERVICE_UUID;
+    const characteristicUuid = import.meta.env.VITE_BLE_CHARACTERISTIC_UUID;
+
     try {
       const device = await navigator.bluetooth.requestDevice({
-        filters: [{ services: ["4655318c-0b41-4725-9c64-44f9fb6098a2"] }],
+        filters: [{ services: [serviceUuid] }],
       });
 
       const res = await axios.get(
@@ -30,12 +33,9 @@ export const AppIndex = () => {
       const token = res.data?.token;
 
       const server = await device.gatt?.connect();
-      const service = await server?.getPrimaryService(
-        import.meta.env.VITE_BLE_SERVICE_UUID,
-      );
-      const characteristic = await service?.getCharacteristic(
-        import.meta.env.VITE_BLE_CHARACTERISTIC_UUID,
-      );
+      const service = await server?.getPrimaryService(serviceUuid);
+      const characteristic =
+        await service?.getCharacteristic(characteristicUuid);
 
       const encodedToken = Buffer.from(token).toString("base64");
       await characteristic?.writeValue(new TextEncoder().encode(encodedToken));
@@ -47,18 +47,18 @@ export const AppIndex = () => {
   };
 
   return (
-    <div className="flex min-h-screen w-full flex-col items-center bg-white">
+    <div className="relative flex min-h-screen w-full flex-col items-center bg-white">
       <Header title="Home" />
-      <main className="relative flex h-full w-full flex-col items-center gap-2 p-2">
+      <main className="flex h-full w-full flex-col items-center gap-2 p-2">
         <RoomSelector />
         <Dashboard />
         {!!navigator.bluetooth && !!userData && (
           <Button
             onClick={testBluetooth}
             size="icon"
-            className="absolute bottom-4 right-4 rounded-full hover:bg-primary focus:bg-primary"
+            className="fixed bottom-4 right-4 flex h-14 w-14 items-center justify-center rounded-full p-2 hover:bg-primary focus:bg-primary"
           >
-            <Bluetooth size="24" />
+            <img src="/images/access-logo.svg" alt="Logo de CSI PRO Access" />
           </Button>
         )}
       </main>
