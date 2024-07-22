f2b9792fdc17297d92dcc5705d58b6ab3d868bec
saulprl <phasecross@hotmail.com>
2024-07-21 22:27:08 -0700

    Add notificationclick event listener

--- public/sw/push.js
+++ public/sw/push.js
@@ -1,4 +1,5 @@
 self.addEventListener("push", onPush);
+self.addEventListener("notificationclick", onNotificationClick);
 
 async function onPush(event) {
   if (event.data) {
@@ -16,4 +17,33 @@ async function onPush(event) {
       }),
     );
   }
+}
+
+async function onNotificationClick(event) {
+  const urlToOpen = new URL(self.location.origin).href;
+
+  const promiseChain = clients
+    .matchAll({
+      type: "window",
+      includeUncontrolled: true,
+    })
+    .then((windowClients) => {
+      let matchingClient = null;
+
+      for (let i = 0; i < windowClients.length; i++) {
+        const windowClient = windowClients[i];
+        if (windowClient.url === urlToOpen) {
+          matchingClient = windowClient;
+          break;
+        }
+      }
+
+      if (matchingClient) {
+        return matchingClient.focus();
+      } else {
+        return clients.openWindow(urlToOpen);
+      }
+    });
+
+  event.waitUntil(promiseChain);
 }
